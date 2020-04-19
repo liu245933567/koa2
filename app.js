65db3ef8a164893f24ccf124c4b4320725d5825f
刘永辉 <liuyonghui@jetair.com.cn>
2020-04-19 17:00:51 +0800

    提交

---
+++ app.js
@@ -0,0 +1,59 @@
+const { MongoClient } = require('mongodb');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'itying';
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+// Use connect method to connect to the Server
+client.connect((err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("连接数据库成功");
+
+  const db = client.db(dbName);
+
+  /** 操作完数据库后一定要关闭 */
+  //  查询
+  // db.collection("user").find({}).toArray((err, data) => {
+  //   console.log(data);
+  //   client.close();
+  // })
+  // 增加
+  // db.collection("user").insertOne({name: '李四', age: 12}, (err, result) => {
+  //   if(err){
+  //     console.log("添加失败")
+  //     return;
+  //   }
+  //   console.log('增加成功');
+  //   console.log(result);
+  //   client.close(); 
+  // })
+
+  // 修改
+  // db.collection('user').updateOne({ name: '张三' }, { $set: { age: 10 } }, (err, result) => {
+  //   if (err) {
+  //     console.log(err);
+  //     return;
+  //   }
+  //   console.log('修改成功');
+  //   console.log(result);
+  //   client.close();
+  // })
+
+  // 删除数据
+  db.collection('user').deleteOne({ name: '李四' }, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('删除一条成功');
+    client.close();
+  })
+});
