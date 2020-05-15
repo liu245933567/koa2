fa394f3374bd921823f064efe71e254fc0f2d82e
刘永辉 <liuyonghui@jetair.com.cn>
2020-05-15 15:28:59 +0800

    修改config配置

--- config/log_config.js
+++ config/log_config.js
@@ -1,52 +1,22 @@
-var path = require('path');
+const path = require('path');
 
 //日志根目录
-var baseLogPath = path.resolve(__dirname, '../logs')
+const baseLogPath = path.resolve(__dirname, '../logs');  
 
-//错误日志目录
-var errorPath = "/error";
+// 错误日志目录
+const errorPath = "/error";  
 //错误日志文件名
-var errorFileName = "error";
+const errorFileName = "error";  
 //错误日志输出完整路径
-var errorLogPath = baseLogPath + errorPath + "/" + errorFileName;
-// var errorLogPath = path.resolve(__dirname, "../logs/error/error");
+const errorLogPath = `${baseLogPath}${errorPath}/${errorFileName}`;  
 
 //响应日志目录
-var responsePath = "/response";
+const responsePath = "/response";
 //响应日志文件名
-var responseFileName = "response";
+const responseFileName = "response";
 //响应日志输出完整路径
-var responseLogPath = baseLogPath + responsePath + "/" + responseFileName;
-// var responseLogPath = path.resolve(__dirname, "../logs/response/response");
+const responseLogPath = `${baseLogPath}${responsePath}/${responseFileName}`;
 
-// module.exports = {
-//   "appenders": [
-//     //错误日志
-//     {
-//       "category": "errorLogger",             //logger名称
-//       "type": "dateFile",                   //日志类型
-//       "filename": errorLogPath,             //日志输出位置
-//       "alwaysIncludePattern": true,          //是否总是有后缀名
-//       "pattern": "-yyyy-MM-dd-hh.log",      //后缀，每小时创建一个新的日志文件
-//       "path": errorPath                     //自定义属性，错误日志的根目录
-//     },
-//     //响应日志
-//     {
-//       "category": "resLogger",
-//       "type": "dateFile",
-//       "filename": responseLogPath,
-//       "alwaysIncludePattern": true,
-//       "pattern": "-yyyy-MM-dd-hh.log",
-//       "path": responsePath
-//     }
-//   ],
-//   //设置logger名称对应的的日志等级
-//   "levels": {
-//     "errorLogger": "ERROR",
-//     "resLogger": "ALL"
-//   },
-//   "baseLogPath": baseLogPath                  //logs根目录
-// }
 module.exports = {
   "replaceConsole": true,
   "appenders": {
@@ -84,5 +54,5 @@ module.exports = {
       "level": "error"
     }
   },
-  "baseLogPath": baseLogPath  
+  "baseLogPath": baseLogPath
 }
