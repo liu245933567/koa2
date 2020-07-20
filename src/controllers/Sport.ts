b4854cb4bfbae970edc26028e5bc735ba13b6b86
刘永辉 <liuyonghui@jetair.com.cn>
2020-07-20 18:33:17 +0800

    feat: 修改景点模拟数据

--- src/controllers/Sport.ts
+++ src/controllers/Sport.ts
@@ -22,6 +22,153 @@ class Sport {
               'https://i.mooool.com/img/2020/03/5c357c0ecb6fc-bp-cover-image.jpg',
             description: ''
           }
+        ],
+        recommends: [
+          {
+            /** 作品名称 */
+            name: '印度南迪亚尔，巴拉吉神庙 / Sameep Padora & Associates',
+            /** 封面图片 */
+            coverImage:
+              'https://i.mooool.com/img/2020/07/20200717155515525.jpg?x-oss-process=style%2Fboombox_image360x270-2x',
+            /** 作品链接 */
+            href: '/work/expmale',
+            /** 作品标签 */
+            type: {
+              text: '景观',
+              href: '/work/expmale'
+            },
+            /** 作品分类 */
+            category: {
+              text: '纪念性空间',
+              href: '/work/expmale'
+            },
+            /** 作品地区 */
+            region: {
+              text: '墨西哥',
+              href: '/work/expmale'
+            },
+            /** 作者 */
+            author: {
+              text: 'JIM STUDIO',
+              href: '/work/expmale'
+            }
+          },
+          {
+            /** 作品名称 */
+            name: '印度南迪亚尔，巴拉吉神庙 / Sameep Padora & Associates',
+            /** 封面图片 */
+            coverImage:
+              'https://i.mooool.com/img/2020/07/20200717155515525.jpg?x-oss-process=style%2Fboombox_image360x270-2x',
+            /** 作品链接 */
+            href: '/work/expmale',
+            /** 作品标签 */
+            type: {
+              text: '景观',
+              href: '/work/expmale'
+            },
+            /** 作品分类 */
+            category: {
+              text: '纪念性空间',
+              href: '/work/expmale'
+            },
+            /** 作品地区 */
+            region: {
+              text: '墨西哥',
+              href: '/work/expmale'
+            },
+            /** 作者 */
+            author: {
+              text: 'JIM STUDIO',
+              href: '/work/expmale'
+            }
+          },
+          {
+            /** 作品名称 */
+            name: '印度南迪亚尔，巴拉吉神庙 / Sameep Padora & Associates',
+            /** 封面图片 */
+            coverImage:
+              'https://i.mooool.com/img/2020/07/20200717155515525.jpg?x-oss-process=style%2Fboombox_image360x270-2x',
+            /** 作品链接 */
+            href: '/work/expmale',
+            /** 作品标签 */
+            type: {
+              text: '景观',
+              href: '/work/expmale'
+            },
+            /** 作品分类 */
+            category: {
+              text: '纪念性空间',
+              href: '/work/expmale'
+            },
+            /** 作品地区 */
+            region: {
+              text: '墨西哥',
+              href: '/work/expmale'
+            },
+            /** 作者 */
+            author: {
+              text: 'JIM STUDIO',
+              href: '/work/expmale'
+            }
+          },
+          {
+            /** 作品名称 */
+            name: '印度南迪亚尔，巴拉吉神庙 / Sameep Padora & Associates',
+            /** 封面图片 */
+            coverImage:
+              'https://i.mooool.com/img/2020/07/20200717155515525.jpg?x-oss-process=style%2Fboombox_image360x270-2x',
+            /** 作品链接 */
+            href: '/work/expmale',
+            /** 作品标签 */
+            type: {
+              text: '景观',
+              href: '/work/expmale'
+            },
+            /** 作品分类 */
+            category: {
+              text: '纪念性空间',
+              href: '/work/expmale'
+            },
+            /** 作品地区 */
+            region: {
+              text: '墨西哥',
+              href: '/work/expmale'
+            },
+            /** 作者 */
+            author: {
+              text: 'JIM STUDIO',
+              href: '/work/expmale'
+            }
+          },
+          {
+            /** 作品名称 */
+            name: '印度南迪亚尔，巴拉吉神庙 / Sameep Padora & Associates',
+            /** 封面图片 */
+            coverImage:
+              'https://i.mooool.com/img/2020/07/20200717155515525.jpg?x-oss-process=style%2Fboombox_image360x270-2x',
+            /** 作品链接 */
+            href: '/work/expmale',
+            /** 作品标签 */
+            type: {
+              text: '景观',
+              href: '/work/expmale'
+            },
+            /** 作品分类 */
+            category: {
+              text: '纪念性空间',
+              href: '/work/expmale'
+            },
+            /** 作品地区 */
+            region: {
+              text: '墨西哥',
+              href: '/work/expmale'
+            },
+            /** 作者 */
+            author: {
+              text: 'JIM STUDIO',
+              href: '/work/expmale'
+            }
+          }
         ]
       }
     };
